310a43a415bc0655ba397d145d70498762de2a07
Lais Portela <lais3722@gmail.com>
2024-04-09 22:58:47 -0300

    colocando minas

--- campo_minado.py
+++ campo_minado.py
@@ -1,9 +1,14 @@
 import tkinter as tk
+import tkinter.messagebox as messagebox
 
+import random
 
 class Color:
     gray_light = '#CDC9C9'
-    border = '#2F4F4F'
+    border = '#2F4F4F',
+    red = '#FF0000'
+
+    
 
 class CampoMInado:
     def __init__(self, minefield, rows, cols, n_mines):
@@ -11,11 +16,18 @@ class CampoMInado:
         self.rows = rows
         self.n_mines = n_mines
         self.cols = cols
-        self.board = [-1 * cols for i in range(rows)]
+        self.board = [[0] * cols for i in range(rows)]
         self.cells = []
-        self.createBoard()
+        self.CreateMines()
+        self.CreateBoard()
         
-    def createBoard(self):
+    def CreateMines(self):
+        for _ in range(self.n_mines):
+            row = random.randint(0, self.rows - 1)
+            col = random.randint(0, self.cols - 1)
+            self.board[row][col] = 1
+        
+    def CreateBoard(self):
         for row in range(self.rows):
             cel_list = []
             for col in range(self.cols):
@@ -30,7 +42,10 @@ class CampoMInado:
             self.cells.append(cel_list)
         
     def reveal(self, row, col):
-        print("reveal")  
+        if self.board[row][col] == 1:
+            self.cells[row][col].config(text='BUM!', bg=Color.red, state='disabled', )
+            messagebox.showinfo("Fim de jogo", "Você perdeu! Tente novamente!")
+            self.mine_field.quit() 
     
     
 minefield = tk.Tk()
