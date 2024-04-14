285ad3a13350064c05a0bbf76ad414350501044a
Lais Portela <lais3722@gmail.com>
2024-04-13 21:13:11 -0300

    finalizando exercicio

--- exercicio.cpp
+++ exercicio.cpp
@@ -4,6 +4,7 @@ using namespace std;
 const int oo = 1e9;
 
 vector<int> bfs(int s, int N, vector<vector<int>> &adj) {
+    // armazena a distância de s para os outros nós
     vector<int> dist(N + 1, -1);
     queue<int> q;
 
@@ -15,22 +16,24 @@ vector<int> bfs(int s, int N, vector<vector<int>> &adj) {
         q.pop();
 
         for (auto v : adj[u]) {
-            if (dist[v] == -1) { 
-                dist[v] = dist[u] + 1;
+            if (dist[v] == -1) { // ainda não visitado
+                dist[v] = dist[u] + 1; // atualiza a distância
                 q.push(v);
             }
         }
     }
 
-    return dist; 
+    return dist; // retorna distancia
 }
 
 int main() {
     int n, value;
     cin >> n;
 
+    // lista de adjacencia 
     vector<vector<int>> pi(n + 1);
 
+    // leitura das relações de funcionários, até n 
     for (int i = 1; i <= n; i++) {
         cin >> value;
 
@@ -41,11 +44,17 @@ int main() {
 
     int altmax = 0;
 
+    // calculo para cada funcionário
     for (int root = 1; root <= n; ++root) {
+        // cria uma bfs para calcular a distâncias 
         vector<int> height = bfs(root, n, pi);
+        // encontra ao número de camadas da árvore ( altura)
+        int x = *max_element(height.begin(), height.end());
+        altmax = max(altmax, x);
     }
 
-
+    // imprime o número de camadas
+    cout << altmax+1 << endl;
 
     return 0;
 }
